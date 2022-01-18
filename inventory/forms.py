2bdf0626104fcf59a9039c9a1e6ff1ee57bbcfa8
WFERRIE <ferriewi@gmail.com>
2022-01-17 23:46:57 -0500

    fixed how data is received in item forms

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,30 +1,21 @@
 from flask_wtf import FlaskForm
 from werkzeug.routing import ValidationError
 from wtforms import StringField, SubmitField, DecimalField, IntegerField
-from wtforms.validators import Length, DataRequired, ValidationError
+from wtforms.validators import Length, DataRequired, ValidationError, NumberRange
 from inventory.models import Item
 
 class CreateItemForm(FlaskForm):
     name = StringField(label = 'Name', validators = [Length(min=2, max=30), DataRequired()])
     price = DecimalField(label = 'Price', validators = [DataRequired()])
     quantity = IntegerField(label = 'Quantity', validators = [DataRequired()])
-    barcode = StringField(label = 'Barcode', validators = [DataRequired()])
+    barcode = IntegerField(label = 'Barcode', validators = [NumberRange(min = 0, max = 999999999999), DataRequired()])
     description = StringField(label = 'Description', validators = [Length(min=2, max=1024), DataRequired()])
     submitCreating = SubmitField(label = "Create Item")
-    
-    def validate_unique(self, parameter, value):
-        if Item.query.filter_by(parameter = value).first():
-            raise ValidationError(f"{parameter} must be Unique.")
 
 
 class DeleteItemForm(FlaskForm):
     submitDeleting = SubmitField(label = "Delete Item")
 
 
-class EditItemForm(FlaskForm):
-    name = StringField(label = 'Name', validators = [Length(min=2, max=30), DataRequired()])
-    price = StringField(label = 'Price', validators = [DataRequired()])
-    quantity = StringField(label = 'Quantity', validators = [DataRequired()])
-    barcode = StringField(label = 'Barcode', validators = [DataRequired()])
-    description = StringField(label = 'Description', validators = [Length(min=2, max=1024), DataRequired()])
+class EditItemForm(CreateItemForm):
     submitEditing = SubmitField(label = "Save Changes")
