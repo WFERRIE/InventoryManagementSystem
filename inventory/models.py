ed401b0e9fb43b67adc9ca563adfde51d6f49c19
WFERRIE <ferriewi@gmail.com>
2022-01-18 23:52:29 -0500

    removed some redundant code

--- inventory/models.py
+++ inventory/models.py
@@ -1,22 +1,4 @@
 from inventory import db
-from flask import send_file
-import sqlite3
-import csv
-
-def exportFile():
-    con = sqlite3.connect('inventory/inventory.db')
-    outfile = open('export.csv', 'w', newline = '')
-    outcsv = csv.writer(outfile)
-
-    cursor = con.execute('select * from item')
-
-    # outcsv.writerow(x[0] for x in cursor.description)
-    outcsv.writerows(cursor.fetchall())
-
-    outfile.close()
-    con.close()
-    return send_file('../export.csv', as_attachment = True, attachment_filename = 'export.csv', mimetype = "text/csv")
-
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
     name = db.Column(db.String(length = 30), nullable = False)
